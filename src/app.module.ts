565096dc0361e926b7ac2238fd9f91b3e702b07f
spyder-s <sandeepsingh121997@gmail.com>
2024-06-10 22:18:49 +0530

    env intro

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { AppService } from './app.service';
 import { RolesService } from './modules/roles/roles.service';
 
 import { UserModule } from './modules/user/user.module';
-import { RolesModule } from './modules/roles/roles.module'
+import { RolesModule } from './modules/roles/roles.module';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,14 +1,21 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'sandeep12',
-    database: 'nestjs',
-    entities: [
-        __dirname + '/../**/*.entity{.ts,.js}',
-    ],
-    synchronize: true,
-}
+  type: process.env.DB_CONNECTION as
+    | 'mysql'
+    | 'postgres'
+    | 'sqlite'
+    | 'mariadb'
+    | 'mongodb'
+    | 'mssql', // Specify allowed types
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10), // Ensure port is a number
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
